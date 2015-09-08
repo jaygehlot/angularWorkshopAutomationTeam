7e41ab34e61b6139dc7a027066e67298a8eaa21c
C1948957_Deeppankur_Tyagi <c1948957@MB523.local>
2015-09-08 15:39:25 +0100

    first commit

---
+++ client/app/scripts/controllers/movie-view.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MovieViewCtrl
+ * @description
+ * # MovieViewCtrl
+ * Controller of the clientApp
+ */
+angular.module('clientApp')
+.controller('MovieViewCtrl', function (
+  $scope,
+  $routeParams,
+  Movie
+) {
+  $scope.viewMovie = true;
+  $scope.movie = Movie.one($routeParams.id).get().$object;
+})
+.directive('youtube', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        src: '='
+      },
+      templateUrl: 'views/youtube.html'
+    };
+  })
+  .filter('trusted', function ($sce) {
+    return function(url) {
+      return $sce.trustAsResourceUrl(url);
+    };
+  });
